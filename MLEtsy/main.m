cca74634d895a39c21f51857a36e68b9df47dba2
shirmung <shirmung@gmail.com>
2011-12-05 18:48:32 -0500

    random item data, updated xcode project

--- MLEtsy/main.m
+++ MLEtsy/main.m
@@ -9,12 +9,15 @@
 #import <Foundation/Foundation.h>
 #import "SBJson.h"
 #import "Key.h"
+#include <stdlib.h>
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
     Key *newKey = [[Key alloc] init];
+    
+    // SPECIFIC USER
     
     NSMutableArray *allListingIDs = [[NSMutableArray alloc] init];
     
@@ -42,14 +45,91 @@ int main (int argc, const char * argv[])
         
         NSDictionary *listingResultDictionary = [listingResult JSONValue];
         
-        // check if listing is unavailable or not (ie: this would mean no tags)
-        if (![[[[listingResultDictionary valueForKey:@"results"] valueForKey:@"state"] objectAtIndex:0] isEqualToString:@"unavailable"]) 
+        // check if listing is active or sold out (ie: this means tags)
+        if ([[[[listingResultDictionary valueForKey:@"results"] valueForKey:@"state"] objectAtIndex:0] isEqualToString:@"sold_out"] || [[[[listingResultDictionary valueForKey:@"results"] valueForKey:@"state"] objectAtIndex:0] isEqualToString:@"active"] ) 
         {
             NSArray *listingTags = [[[listingResultDictionary valueForKey:@"results"] valueForKey:@"tags"] objectAtIndex:0];
         
-            //print listing ID
+            // print listing ID
             NSLog(@"%@", [NSString stringWithFormat:@"%@:", [allListingIDs objectAtIndex:listingID]]);
   
+            for (int listingTag = 0; listingTag < [listingTags count]; listingTag++) 
+            {
+                // print listing tags
+                NSLog(@"%@", [listingTags objectAtIndex:listingTag]);
+            }
+        }
+    }
+    
+    // RANDOM LISTINGS
+    
+    NSMutableArray *usedRandomNumbers = [[NSMutableArray alloc] init];
+    
+    NSMutableArray *allRandomListingIDs = [[NSMutableArray alloc] init];
+    
+    // getting 8000 random listings
+    while ([allRandomListingIDs count] != 8000) 
+    {
+        NSString *randomNumber = @"";
+        
+        for (int i = 0; i < 8; i++) 
+        {
+            int randomDigit = arc4random() % 9;
+            
+            NSString *intString = [NSString stringWithFormat:@"%i", randomDigit];
+            randomNumber = [randomNumber stringByAppendingString:intString];
+        }
+        
+        int randomListingID = [randomNumber intValue];
+        
+        NSString *randomListingRequest = [NSString stringWithFormat:@"http://openapi.etsy.com/v2/listings/%i?api_key=%@", randomListingID, newKey.myKey];
+        NSString *randomListingResult = [NSString stringWithContentsOfURL:[NSURL URLWithString:randomListingRequest] encoding:NSUTF8StringEncoding error:NULL];
+        
+        if ([randomListingResult isEqualToString:[NSString stringWithFormat:@"Translation_TranslatableFinder: Listing-&gt;find(%i) does not exist", randomListingID]]) {
+            NSLog(@"this listing does not exist");
+        } else {
+            NSDictionary *randomListingResultDictionary = [randomListingResult JSONValue];
+
+            // for each of the entries
+            for (int i = 0; i < [[randomListingResultDictionary valueForKey:@"results"] count]; i++) 
+            {
+                BOOL present = FALSE;
+                
+                for (NSString* aRandomNumber in usedRandomNumbers)
+                {
+                    if ([aRandomNumber isEqualToString:randomNumber]) 
+                    {
+                        present = TRUE;
+                    }
+                }
+                
+                if (present == FALSE) {
+                    [allRandomListingIDs addObject:[[[randomListingResultDictionary valueForKey:@"results"] valueForKey:@"listing_id"] objectAtIndex:i]];
+                    [usedRandomNumbers addObject:randomNumber];
+                } else {
+                    NSLog(@"this listing already has been accounted for");
+                }
+            }
+        }
+    }
+    
+    // for all of the listing IDs
+    for (int listingID = 0; listingID < [allRandomListingIDs count]; listingID++) 
+    {
+        // listing request
+        NSString *listingRequest = [NSString stringWithFormat:@"http://openapi.etsy.com/v2/listings/%i?api_key=%@", [[allRandomListingIDs objectAtIndex:listingID] intValue], newKey.myKey];
+        NSString *listingResult = [NSString stringWithContentsOfURL:[NSURL URLWithString:listingRequest] encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSDictionary *listingResultDictionary = [listingResult JSONValue];
+        
+        // check if listing is active or sold out (ie: this means tags)
+        if ([[[[listingResultDictionary valueForKey:@"results"] valueForKey:@"state"] objectAtIndex:0] isEqualToString:@"sold_out"] || [[[[listingResultDictionary valueForKey:@"results"] valueForKey:@"state"] objectAtIndex:0] isEqualToString:@"active"] ) 
+        {
+            NSArray *listingTags = [[[listingResultDictionary valueForKey:@"results"] valueForKey:@"tags"] objectAtIndex:0];
+            
+            //print listing ID
+            NSLog(@"%@", [NSString stringWithFormat:@"%@:", [allRandomListingIDs objectAtIndex:listingID]]);
+            
             for (int listingTag = 0; listingTag < [listingTags count]; listingTag++) 
             {
                 //print listing tags
@@ -57,8 +137,7 @@ int main (int argc, const char * argv[])
             }
         }
     }
-
+    
     [pool drain];
     return 0;
 }
-
