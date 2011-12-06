3a670c1bdea3710e26eaec65f57330d66065af43
shirmung <shirmung@gmail.com>
2011-12-05 20:17:15 -0500

    modifying tester.java and added randomitems.java

---
+++ java/src/RandomItems.java
@@ -0,0 +1,78 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class RandomItems {
+    
+    public ArrayList<RandomItem> items;
+    public HashMap<Integer, Integer> idDict;
+    public HashMap<String, Integer> tagDict;
+
+    public RandomItems() {
+        items = new ArrayList<RandomItem>();
+        idDict = new HashMap<Integer, Integer>();
+        tagDict = new HashMap<String, Integer>();
+    }
+    
+    public RandomItems(String filePath) {
+        this();
+        
+        this.process(filePath);
+    }
+    
+    // method takes the name of a text file formated as ItemID:tag,tag,tag...
+    public void process(String filePath) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));    
+            
+            String thisLine;
+            
+            while((thisLine = reader.readLine()) != null) {
+                // make a new item
+                RandomItem tempItem = new RandomItem(thisLine);
+                
+                // put this item in the arrayList if it has not been parsed yet
+                if (!idDict.containsKey(tempItem.id)) {
+                    idDict.put(tempItem.id, tempItem.id);
+                    items.add(tempItem);  
+                    
+                    // get tag counts from the item
+                    for (String tag : tempItem.getTags()) {
+                        int count = 1;
+                        
+                        if (tagDict.containsKey(tag)) {
+                            count = count + tagDict.get(tag);
+                        }
+                        
+                        tagDict.put(tag, count);
+                    }
+                } else {
+                    System.out.println("Repeat item: " + tempItem.id);
+                }
+            }
+            
+            reader.close();
+        } catch (IOException e) {
+            
+        }
+    }
+	
+    public String toString() {
+        return tagDict.toString();
+    }
+    
+    public ArrayList<RandomItem> getItems() {
+        return items;
+    }
+    
+    // parse through data, record 
+    public static void main(String args[]) {
+        RandomItems ourRandomItems = new RandomItems("randomItems.txt");
+
+        System.out.println("Num Items: " + ourRandomItems.items.size());
+    }
+}
--- java/src/Tester.java
+++ java/src/Tester.java
@@ -5,22 +5,47 @@ public class Tester {
     
     // Instance variables
     public User user;
-    public ArrayList<RandomItem> randomItems;
-    public int n; // number of nearest neighbors
-    public HashMap<Item, Double> itemsSimilarityScore;
-    
+    public RandomItems randomItems;
+
     // This assigns each tag value a unique integer
-    public HashMap<String, Integer> tagDict;
+    public HashMap<String, Integer> allTagsDict;
+
+    public HashMap<Item, Double> itemsSimilarityScore;
+    public int n; // number of nearest neighbors
     
     public Tester() {
         user = new User();
-        randomItems = new ArrayList<Item>();
-        N = 20;
+        randomItems = new RandomItems();
+        
+        allTagsDict = new HashMap<String, Integer>();
+        
+        itemsSimilarityScore = new HashMap<Item, Double>();
+        n = 20;
     }
     
-    public Tester(User user) {
+    public Tester(User user, RandomItems randomItems) {
         this();
+        
         this.user = user;
+        this.randomItems = randomItems;
+        
+        this.allTagsDict = this.prepareAllTagsDict();
+    }
+    
+    public HashMap<String,Integer> prepareAllTagsDict() {
+        HashMap<String, Integer> temp = new HashMap<String, Integer>();
+        
+        for (String key : this.user.tagDict) {
+            temp.put(key, (temp.size() - 1));
+        }
+        
+        for (String key : this.randomItems.tagDict) {
+            if (!temp.containsKey(key)) {
+                temp.put(key, (temp.size() - 1));
+            }
+        }
+        
+        return temp;
     }
     
     // algorithm:
@@ -28,21 +53,18 @@ public class Tester {
     // find the top 20 most correlated vectors;
     // for each item in randomitem, calculate the score
     // returns a sorted array of items
-    
     public ArrayList<Item> preferences() {
         // for each user's item, find closest random items
         for (UserItem userItem : user.getItems()) {
             userItem.findClosestItems(randomItems);
         }
+        
         // for each random item, calculate preference score
         for (RandomItem item : randomItems) {
             findPreferenceScore();
         }
+        
         // sort the randomItems by itemsScore
-        
-        
     }
-    
-    
-    
+
 }
