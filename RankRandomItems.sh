a80d2803938d1c72f60423a97ff38dc73350d1eb
Howard Jing <howard.jing@gmail.com>
2011-12-09 20:46:33 -0500

    results are in the output foloder

--- RankRandomItems.sh
+++ RankRandomItems.sh
@@ -5,7 +5,11 @@ set -e # stop execution if there is an error
 javac -sourcepath java/src -d java/classes java/src/RankRandomItems.java
 
 # run it
-userItemsPath=$1
-randomItemsPath=$2
-outputResultsPath=$3
-java -Xmx2048m -classpath java/classes RankRandomItems ${userItemsPath} ${randomItemsPath} > ${outputResultsPath}
+#userItemsPath=$1
+#randomItemsPath=$2
+#outputResultsPath=$3
+#java -Xmx2048m -classpath java/classes RankRandomItems ${userItemsPath} ${randomItemsPath} > ${outputResultsPath}
+
+for i in {0..9}; do
+    java -Xmx2048m -classpath java/classes RankRandomItems data/trainingnot${i}.txt data/test${i}.txt > output/results${i}.txt
+done
