2a2dc2f0f842c25583a562ce49969d6adc6eee0b
Howard Jing <howard.jing@gmail.com>
2011-12-10 23:45:50 -0500

    training data

--- RankRandomItemsScript.sh
+++ RankRandomItemsScript.sh
@@ -3,6 +3,7 @@ set -e # stop execution if there is an error
 
 # compile RankRandomItems
 javac -sourcepath java/src -d java/classes java/src/RankRandomItems.java
+javac -sourcepath java/src -d java/classes java/src/RankRandomFilteredItems.java
 
 # run it
 # userItemsPath=$1
@@ -10,7 +11,12 @@ javac -sourcepath java/src -d java/classes java/src/RankRandomItems.java
 # outputResultsPath=$3
 # java -Xmx2048m -classpath java/classes RankRandomItems ${userItemsPath} ${randomItemsPath} > ${outputResultsPath}
 
-for i in {0..9}; do
+for i in {1..4}; do
     echo "processing ${i}..."
-    java -Xmx3072m -classpath java/classes RankRandomItems data/trainingnot${i}.txt data/test${i}.txt > output/results${i}.txt
+    for j in {1..4}; do
+        if [ ${i} -ne ${j} ]; then
+            java -Xmx3072m -classpath java/classes RankRandomItems data/splits/splits${i} data/splits/test${j} > output/results${i}${j}
+            java -Xmx3072m -classpath java/classes RankRandomFilteredItems data/splits/splits${i} data/splits/test${j} > output/filteredResults${i}${j}
+        fi
+    done
 done
